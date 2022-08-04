12702ea9f7d715104b010a51768e08edb260a2ab
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-04 10:25:03 +0800

    init repo

---
+++ src/App.svelte
@@ -0,0 +1,130 @@
+<script lang="ts">
+  import Navbar from "./lib/Navbar.svelte";
+</script>
+
+<main>
+  <Navbar />
+  <div style="margin-top:75px;">
+    <h1>Hello, world</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis placeat
+      nobis, quod nesciunt ex perferendis autem iure magni numquam voluptate?
+    </p>
+    <p>
+      Fugit, in temporibus, laboriosam esse error debitis, dolorem placeat sint
+      corrupti et illo modi ab quod blanditiis odit reprehenderit veniam?
+    </p>
+    <p>
+      Suscipit consectetur minima qui tempora quo ad ipsam ab rerum, hic
+      eligendi cumque perspiciatis eveniet ducimus repudiandae distinctio
+      perferendis sit!
+    </p>
+    <p>
+      Ad nesciunt nam accusamus. Voluptatum, soluta expedita repellendus sint
+      possimus aspernatur ducimus, consequatur magni quos atque tempore
+      praesentium? Aliquam, officia!
+    </p>
+    <p>
+      Aperiam eaque illum veritatis fugit, facere expedita ad dolores omnis
+      voluptas cupiditate soluta, ratione at harum perspiciatis repellat
+      corrupti eum.
+    </p>
+    <p>
+      Aperiam eaque illum veritatis fugit, facere expedita ad dolores omnis
+      voluptas cupiditate soluta, ratione at harum perspiciatis repellat
+      corrupti eum.
+    </p>
+    <p>
+      Aperiam eaque illum veritatis fugit, facere expedita ad dolores omnis
+      voluptas cupiditate soluta, ratione at harum perspiciatis repellat
+      corrupti eum.
+    </p>
+    <p>
+      Aperiam eaque illum veritatis fugit, facere expedita ad dolores omnis
+      voluptas cupiditate soluta, ratione at harum perspiciatis repellat
+      corrupti eum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
+      doloribus at corrupti accusamus adipisci. Animi eius illo laborum fugit in
+      excepturi pariatur assumenda minima nemo ipsa, nisi cum cupiditate, illum
+      alias? Sint dolore rem maiores officiis quod quae veritatis ea, hic,
+      beatae, sit aliquam cumque ad laudantium totam ipsa a.
+    </p>
+    <p>
+      Voluptatem dicta numquam eius quos libero? Aspernatur, sunt accusantium
+      tempore, iusto alias, dolor deleniti necessitatibus rerum sequi at ipsa?
+      Sequi laudantium, natus nesciunt quam iure et itaque maiores optio ducimus
+      libero, laboriosam officiis saepe. Perspiciatis, nostrum assumenda
+      inventore dolore sapiente fugiat optio reprehenderit, quidem atque facilis
+      sit cumque animi unde.
+    </p>
+    <p>
+      Velit doloribus mollitia laboriosam qui. Autem exercitationem pariatur,
+      vitae similique blanditiis corrupti rem, fugit expedita cumque molestiae
+      reprehenderit incidunt? Molestiae ducimus distinctio minima autem dolore
+      impedit in consectetur numquam corporis adipisci maiores rem maxime dicta,
+      accusamus a, quasi doloribus animi et laboriosam officiis! Tempore error
+      quisquam eligendi ab debitis tempora!
+    </p>
+    <p>
+      Sed minus tempore, excepturi animi, atque suscipit eligendi numquam libero
+      vitae inventore harum fugiat nam voluptate laborum quam praesentium modi.
+      Sit, reiciendis possimus! Dolor voluptatibus ullam eos nulla nam commodi
+      quas nesciunt, explicabo cupiditate eaque dignissimos quisquam, laudantium
+      ipsa maiores incidunt ipsam natus. Rem quos iusto magnam tempore, optio
+      maxime!
+    </p>
+    <p>
+      Rerum, debitis. Saepe praesentium reprehenderit ex sint quos quia alias
+      illum nobis aliquam laudantium, repellat amet ratione odio, aliquid culpa
+      incidunt nulla error ullam, cupiditate autem officiis enim? Sapiente est
+      recusandae molestias totam accusamus explicabo, quam minus perferendis
+      dolor non distinctio earum sequi id illo voluptatum nostrum, blanditiis,
+      eius accusantium!
+    </p>
+    <p>
+      Ipsa, consequatur quam adipisci nisi saepe error ipsum provident, sunt
+      eaque cum sit, modi repudiandae vel nemo officia. Deserunt, fugit, nulla
+      minima enim, hic eaque ipsam vitae aliquam dignissimos necessitatibus
+      laboriosam libero saepe expedita quo id voluptatum aspernatur unde
+      similique delectus incidunt quia! Reprehenderit aut ratione ad sequi
+      nesciunt? Quas!
+    </p>
+    <p>
+      Neque fugit quae ratione quod unde molestiae, totam dignissimos mollitia
+      tempora illo beatae blanditiis aspernatur voluptatibus earum? Voluptates
+      iste repellat magnam corporis eos repellendus accusantium pariatur
+      quaerat, optio nemo quos et est dolore hic ratione aliquid laudantium
+      sapiente animi minima consequatur tenetur nesciunt voluptas. Sunt
+      doloremque ipsum facilis assumenda consectetur.
+    </p>
+    <p>
+      Dignissimos, quo molestiae? Voluptas optio molestias dicta, aperiam saepe
+      facere minima cumque atque ipsam laudantium pariatur, quas veritatis
+      laboriosam, ratione asperiores tenetur odit possimus ipsa nesciunt nemo
+      fugiat? Non quibusdam, sequi possimus nam maxime iste consequuntur,
+      reiciendis provident vitae corporis reprehenderit, iusto dignissimos!
+      Accusamus tenetur et, iure laborum enim perferendis?
+    </p>
+    <p>
+      Adipisci ex nihil aliquam deleniti quia, rem praesentium, voluptate
+      dignissimos, perferendis nisi voluptas? Temporibus placeat sit optio,
+      velit quibusdam incidunt quisquam id doloremque, voluptate repudiandae,
+      maiores quasi qui impedit. Quos libero optio dignissimos facere numquam
+      facilis id ea tenetur eius eum incidunt nihil tempore, voluptatibus
+      consequuntur corporis? Assumenda, eveniet nam.
+    </p>
+    <p>
+      Nemo consectetur ut similique doloremque possimus. Nulla quia
+      reprehenderit asperiores est repellat deserunt? Repellat deleniti,
+      reprehenderit commodi natus, earum ea et culpa architecto quisquam sint
+      sunt modi molestiae, vero quos. Cumque, soluta quae placeat nemo sint
+      optio facere saepe tempore ipsum cum explicabo dolor fuga mollitia
+      voluptates? Explicabo, maxime. Magni.
+    </p>
+  </div>
+</main>
+
+<style>
+</style>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  let isSticky = false;
+  onMount(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        isSticky = true;
+        return;
+      }
+      isSticky = false;
+    });
+  });
+</script>
+
+<nav class:sticky={isSticky}>
+  <ul>
+    <li>
+      <a href="#">Home</a>
+    </li>
+    <li>
+      <a href="#">About</a>
+    </li>
+    <li>
+      <a href="#">Contact</a>
+    </li>
+  </ul>
+</nav>
+
+<style>
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+  }
+  ul > li {
+    margin-right: 0.5rem;
+  }
+  nav {
+    color: #fff;
+    padding: 0.5rem;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
+  }
+  nav a {
+    color: #333;
+    text-decoration: none;
+    padding: 0.5rem 1rem;
+    display: block;
+  }
+  nav.sticky a {
+    border-radius: 0.5rem;
+    transition-property: color, background;
+    transition-duration: 75ms;
+    transition-timing-function: ease-in-out;
+  }
+  nav.sticky a:hover {
+    background: #fff;
+    color: #333;
+  }
+  nav.sticky {
+    background: #fff;
+    position: fixed;
+    animation: slide-down 0.175s ease-out forwards;
+  }
+  @keyframes slide-down {
+    from {
+      transform: translateY(-100%);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+</style>
